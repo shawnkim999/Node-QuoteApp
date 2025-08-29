8c345e0a1acc3ee5d716104a960b6b33387d1546
shawnkim999 <shawnkimprojects@gmail.com>
2025-08-28 19:01:57 -0700

    Building frontend and fixing cors

--- API/src/app.ts
+++ API/src/app.ts
@@ -1,16 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
 import quotesRoutes from './routes/quotesRoutes';
 
 const app = express();
+app.use(cors())
 
 app.use(express.json());
 app.use("/quotes", quotesRoutes);
 
-const PORT = 3000;
+const PORT = 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
 })
---
+++ client/src/app/page.tsx
@@ -0,0 +1,80 @@
+"use client"
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+interface Quote {
+  id: string;
+  text: string;
+  author: string;
+  likes: number;
+}
+
+export default function Home() {
+  const [quotes, setQuotes] = useState<Quote[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const api = process.env.NEXT_PUBLIC_API_URL;
+
+  useEffect(() => {
+    fetchQuotes();
+  }, [])
+
+  const fetchQuotes = async () => {
+    axios.get(`${api}/quotes`)
+      .then(res => {
+        setQuotes(res.data);
+        setLoading(false);
+      })
+      .catch(err => {
+        console.error(err);
+        setError("Failed to fetch quotes");
+        setLoading(false);
+      })
+  }
+
+  const handleLike = async (id: string) => {
+    try {
+      const res = await axios.post(`${api}/quotes/${id}/like`);
+      const updatedQuote = res.data;
+
+      setQuotes(prev => 
+        prev.map(quote =>
+          quote.id === id ? { ...quote, likes: updatedQuote.likes } : quote
+        )
+      )
+    } catch (err) {
+        console.error("Failed to like quote: ", err);
+    }
+  }
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>{error}</p>
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Author</th>
+            <th>Text</th>
+            <th>Likes</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {quotes.map((quote: any) => (
+            <tr key={quote.id}>
+              <td>{quote.author}</td>
+              <td>{quote.text}</td>
+              <td>{quote.likes}</td>
+              <td>
+                <button onClick={() => handleLike(quote.id)}>❤️ Like</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
